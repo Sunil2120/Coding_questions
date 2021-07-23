99bba9a5bccf9307e985a19605900d8d55c7773c
Sunil2120 <sunilms2120@gmail.com>
2021-07-23 07:24:53 +0530

    changed test3

--- test3.cpp
+++ test3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
+    cout << "print statement" << endl;
     double arr[4] = {14.5, 4.0, 10.7, 78.5};
     double *p1 = &arr[0];
     double *p2 = p1 + 3;
